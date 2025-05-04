b0dbbb7dae9c500b730443edb8fc6e69d934555a
Diana Asmus <121640648+dianaasmus@users.noreply.github.com>
2025-05-04 18:57:56 +0200

    style: set color design patterns

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,19 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <div class="flex flex-col justify-center p-5">
-    <header>
-      <a href="https://vuejs.org/" hreflang="en">
-        <span class="relative block h-10 max-w-10">
-          <img alt="Vue logo" src="./assets/logo.svg" />
-        </span>
-        <span class="sr-only">Visit the Vue.js homepage</span>
-      </a>
+  <div class="flex flex-col justify-center">
+    <header class="flex h-20 items-center justify-center">
+      <nav>
+        <a href="https://vuejs.org/" hreflang="en">
+          <span class="relative block h-10 w-10">
+            <img alt="Vue logo" src="./assets/logo.svg" />
+          </span>
+          <span class="sr-only">Visit the Vue.js homepage</span>
+        </a>
+      </nav>
     </header>
 
-    <main>
+    <main class="p-5 md:p-20">
       <TheWelcome />
     </main>
   </div>
