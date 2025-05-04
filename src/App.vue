378891d5232ede650bfc41701e7e937f39d70f49
Diana Asmus <121640648+dianaasmus@users.noreply.github.com>
2025-05-04 17:34:13 +0200

    feat: add Tailwind CSS integration + header body wrapper

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,18 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <a href="https://vuejs.org/" hreflang="en">
-      <span class="block h-10 relative max-w-10">
-        <img alt="Vue logo" src="./assets/logo.svg" />
-      </span>
-      <span class="sr-only">Visit the Vue.js homepage</span>
-    </a>
-  </header>
+  <div class="flex flex-col justify-center p-5">
+    <header>
+      <a href="https://vuejs.org/" hreflang="en">
+        <span class="relative block h-10 max-w-10">
+          <img alt="Vue logo" src="./assets/logo.svg" />
+        </span>
+        <span class="sr-only">Visit the Vue.js homepage</span>
+      </a>
+    </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+      <TheWelcome />
+    </main>
+  </div>
 </template>
